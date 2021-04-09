78a86a8a4d37ad220b7e9cfed7a104bb53188205
Dev Joe <joe.afify95@gmail.com>
2021-04-09 23:00:29 +0200

    Added fixed position Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { pizzaRed } from "./styles/colors";
+import { pizzaRed } from "../styles/colors";
 
 const NavbarStyled = styled.div`
   background-color: ${pizzaRed};
   padding: 20px;
+  position: fixed;
+  width: 100%;
 `;
 
 const LogoStyled = styled.span`
-font-family: 'Righteous', cursive;
-font-size:20px;
-color: white;
-text-shadow:3px 3px 5px #333;
+  font-family: "Righteous", cursive;
+  font-size: 20px;
+  color: white;
+  text-shadow: 3px 3px 5px #333;
 `;
 const Navbar = (props) => {
   return (
