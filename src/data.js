ba10af6680051528b2a887e94c02173a8b189542
Dev Joe <joe.afify95@gmail.com>
2021-04-10 03:29:49 +0200

    Added dummy menu data.

---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const foods = [
+  {
+    name: "Meat Pizza",
+    img: "images/meat-pizza.jpg",
+    category: "Pizza",
+  },
+  {
+    name: "Sausage Pizza",
+    img: "images/sausage-pizza.jpg",
+    category: "Pizza",
+  },
+  {
+    name: "Peperoni Pizza",
+    img: "images/peperoni-pizza.jpg",
+    category: "Pizza",
+  },
+  {
+    name: "Meat Burger",
+    img: "images/meat-burger.jpg",
+    category: "Burger",
+  },
+  {
+    name: "Chicken Burger",
+    img: "images/chicken-burger.jpg",
+    category: "Burger",
+  },
+  {
+    name: "Chicken Wings",
+    img: "images/chicken-wings.jpg",
+    category: "Chicken",
+  },
+  {
+    name: "Fried Chicken",
+    img: "images/fried-chicken.jpg",
+    category: "Chicken",
+  },
+  {
+    name: "Pasta",
+    img: "images/pasta.jpg",
+    category: "Pasta",
+  },
+  {
+    name: "Noodles",
+    img: "images/noodles.jpg",
+    category: "Noodles",
+  },
+  {
+    name: "French Fried",
+    img: "images/french-fried.jpg",
+    category: "Appetizers",
+  },
+  {
+    name: "Salad",
+    img: "images/salad.jpg",
+    category: "Appetizers",
+  },
+];
