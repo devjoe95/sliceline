8a001ec915da2aa42ef4723861a381e99a52e0f8
Dev Joe <joe.afify95@gmail.com>
2021-04-18 01:41:48 +0200

    Edited button style

--- src/styles/BtnStyled.js
+++ src/styles/BtnStyled.js
@@ -4,10 +4,9 @@ import { pizzaRed } from "./colors";
 export const BtnStyled = styled.button`
   display: block;
   width: 100%;
-  padding: 10px;
+  padding: 15px;
   background-color: ${pizzaRed};
   color: white;
-  border-radius: 5px;
   border: none;
   font-size: 1rem;
   font-weight: bold;
